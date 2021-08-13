b5c07d90b8bd42cd7cf442b6fd3dc75eee364880
PirateBl4ck <rafael.moussa13@gmail.com>
2021-08-13 16:25:17 -0300

    O botão continue do diálogo quebrou
    E n sei oq ta errado

--- JobFreelance/Assets/Scripts/Dialog.cs
+++ JobFreelance/Assets/Scripts/Dialog.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Dialog : MonoBehaviour
@@ -9,19 +10,39 @@ public class Dialog : MonoBehaviour
     public string[] sentences;
     private int index;
     public float typingSpeed;
-
+    public GameObject dialogBox;
     public GameObject continueButton;
+    public bool PlayerInRange;
 
     void Start(){
 
         StartCoroutine(Type());
+        
     }
 
     void Update(){
 
         if(textDisplay.text == sentences[index]){
+            dialogBox.SetActive(true);
             continueButton.SetActive(true);
 
+        }
+    }
+
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            PlayerInRange = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D (Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            PlayerInRange = false;
         }
     }
 
@@ -45,6 +66,7 @@ public class Dialog : MonoBehaviour
         else{
             textDisplay.text = "";
             continueButton.SetActive(false);
+            dialogBox.SetActive(false);
         }
     }
 }
